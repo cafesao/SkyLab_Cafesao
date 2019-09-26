83943f41ee2a4e04d77e5fb3c9772f00e939af6c
cafesao <gabriel.diasdutra2@gmail.com>
2019-09-26 17:24:47 -0300

    Modificando arquivos (Index e Script)

--- JS/SkyLab_JS/Exec01/script.js
+++ JS/SkyLab_JS/Exec01/script.js
@@ -1,23 +1,8 @@
-function checar(){
+function checaIdade(){
     var inputIdade = document.querySelector('input#idade').value
-    
-    function checaIdade(idade){
-        return new Promise(function(resolve, reject){
-            if (idade > 18){
-                resolve('Maior que 18')
-            }
-            else {
-                reject('Menor que 18')
-            }
-        })
+    if (inputIdade > 18) {
+        return console.log('Maior de 18 Anos')
+    } else {
+        return console.log('Menor de 18')
     }
-    
-    checaIdade(inputIdade)
-        .then(function(response){
-            console.log(response)
-        })
-        .catch(function(error){
-            console.log(error)
-        })
 }
-
