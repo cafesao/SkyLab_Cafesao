78b306c6c058e7aa58e42d1173206aa178e697f1
cafesao <gabriel.diasdutra2@gmail.com>
2019-09-27 13:53:00 -0300

    Modificação de Script

--- JS/SkyLab_JS/Exec02/script.js
+++ JS/SkyLab_JS/Exec02/script.js
@@ -41,26 +41,24 @@ function pesquisar(){
         renderizarLISemLink('Carregando...')
     
         axios.get(url)
-            .then(function(response){
+            .then ((response) => {
                 apagarUL()
                 let tamanhoLista = response.data.length
                 usuario.value = ''
                 if (tamanhoLista == 0){
                     renderizarLISemLink('Sem repositório!')
                 }
-                for(let c = 0; c <= tamanhoLista; c++){
-                    var nameLista = response.data[c].name
-                    var link = response.data[c].html_url
+                for(let cont = 0; cont <= tamanhoLista; cont++){
+                    var nameLista = response.data[cont].name
+                    var link = response.data[cont].html_url
                     renderizarLI(nameLista, link)
                 }
             })
-            .catch(function(error){
+            .catch((error) => {
                 if(error == 'Error: Request failed with status code 404'){
                     apagarUL()
                     renderizarLI('Erro 404 - Não existe este USER')
                 }
             })
     }
-
-    
 }
