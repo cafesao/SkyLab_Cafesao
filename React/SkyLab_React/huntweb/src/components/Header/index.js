1016c2ec2206a6907074aa3c916d993cf8a758cf
cafesao <gabriel.diasdutra2@gmail.com>
2019-10-01 09:21:08 -0300

    [CS] Modificando arquivos

--- React/SkyLab_React/huntweb/src/components/Header/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './style.css'
-
-// Se tiver mais linhas adicionar () dps da =>
-const Header = () => <header id='main-header'>JSHunt</header>
-
-export default Header
