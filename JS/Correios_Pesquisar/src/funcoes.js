80c1317a762bd3cd5885285ed4d7f970fd6378d1
cafesao <gabriel.diasdutra2@gmail.com>
2019-10-02 14:53:52 -0300

    [CS] Adicionando Arquivos

--- JS/Correios_Pesquisar/src/funcoes.js
+++ JS/Correios_Pesquisar/src/funcoes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-export {apagarResultado, api}
+export {apagarResultado, apiCep, apiRua}
 const arrayCep = {
     cep: '',
     rua: '',
@@ -7,8 +7,15 @@ const arrayCep = {
     cidade: '',
     estado: ''
 }
+const arrayRua = {
+    cep: '',
+    rua: '',
+    bairro: '',
+    cidade: '',
+    estado: ''
+}
 
-async function api(cepInput, div) {
+async function apiCep(cepInput, div) {
     try {
         carregando(true, div)
         const cepAxios = await axios.get(`https://viacep.com.br/ws/${cepInput}/json/`)
@@ -24,21 +31,48 @@ async function api(cepInput, div) {
             erroCep(div)
         }
         else{
-            adicionarResultado(div)
+            adicionarResultadoCep(div)
         }
     }
     catch{}
 }
 
+async function apiRua(estado, cidade, rua, div) {
+    try{
+        carregando(true, div)
+        const ruaAxios = await axios.get(`https://viacep.com.br/ws/${estado}/${cidade}/${rua}/json/`)
+        const { cep, erro, logradouro, localidade, bairro, uf } = ruaAxios.data[0]
+        arrayRua.cep = cep
+        arrayRua.erro = erro
+        arrayRua.rua = logradouro
+        arrayRua.cidade = localidade
+        arrayRua.bairro = bairro
+        arrayRua.estado = uf
+
+        carregando(false, div)
+        if (arrayRua.erro == true){
+            erroCep(div)
+        }
+        else {
+            adicionarResultadoRua(div)
+        }
+    }
+    catch(error){
+        carregando(false, div)
+        alert('[ERRO] Endereço não encontrado!')
+        console.warn(error)
+    }
+}
+
 function erroCep(div){
-    let erroElemento = document.createElement('h3')
+    let erroElemento = document.createElement('h2')
     let erroTexto = document.createTextNode('O Cep não foi encontrado, tente novamente!')
     erroElemento.appendChild(erroTexto)
     div.appendChild(erroElemento)
     console.warn('[Erro] Cep não encontrado')
 }
 
-function adicionarResultado(div) {
+function adicionarResultadoCep(div) {
     let cepElemento = document.createElement('p')
     let ruaElemento = document.createElement('p')
     let bairroElemento = document.createElement('p')  
@@ -50,6 +84,32 @@ function adicionarResultado(div) {
     let bairroTexto = document.createTextNode(`Bairro: ${arrayCep.bairro}`) 
     let cidadeTexto = document.createTextNode(`Cidade: ${arrayCep.cidade}`)
     let estadoTexto = document.createTextNode(`Estado: ${arrayCep.estado}`) 
+
+    cepElemento.appendChild(cepTexto)
+    ruaElemento.appendChild(ruaTexto)
+    bairroElemento.appendChild(estadoTexto)
+    cidadeElemento.appendChild(bairroTexto)
+    estadoElemento.appendChild(cidadeTexto)
+
+    div.appendChild(cepElemento)
+    div.appendChild(ruaElemento)
+    div.appendChild(bairroElemento)
+    div.appendChild(cidadeElemento)
+    div.appendChild(estadoElemento)
+}
+
+function adicionarResultadoRua(div){
+    let cepElemento = document.createElement('p')
+    let ruaElemento = document.createElement('p')
+    let bairroElemento = document.createElement('p')  
+    let cidadeElemento = document.createElement('p')    
+    let estadoElemento = document.createElement('p')  
+
+    let cepTexto = document.createTextNode(`CEP: ${arrayRua.cep}`)
+    let ruaTexto = document.createTextNode(`Rua: ${arrayRua.rua}`)
+    let bairroTexto = document.createTextNode(`Bairro: ${arrayRua.bairro}`) 
+    let cidadeTexto = document.createTextNode(`Cidade: ${arrayRua.cidade}`)
+    let estadoTexto = document.createTextNode(`Estado: ${arrayRua.estado}`) 
 
     cepElemento.appendChild(cepTexto)
     ruaElemento.appendChild(ruaTexto)
--- JS/Correios_Pesquisar/src/main.js
+++ JS/Correios_Pesquisar/src/main.js
@@ -1,19 +1,40 @@
 // Modulos
-import {apagarResultado, api} from "./funcoes"
+import {apagarResultado, apiCep, apiRua} from "./funcoes"
 
 // Variaveis
-var input = document.querySelector('input#cep')
-var botaoResultado = document.querySelector('button#resultado')
+var inputCep = document.querySelector('input#cep')
+var inputRua = document.querySelector('input#rua')
+var inputCidade = document.querySelector('input#cidade')
+var inputEstado = document.querySelector('input#estado')
+var botaoResultadoCep = document.querySelector('button#resultadoCep')
+var botaoResultadoRua = document.querySelector('button#resultadoRua')
 var div = document.querySelector('div#resultado')
-botaoResultado.addEventListener("click", verificar)
+botaoResultadoCep.addEventListener("click", verificarCep)
+botaoResultadoRua.addEventListener("click", verificarRua)
+
 
 // Funções
-function verificar(){
-    if (input.value.length < 8){
+function verificarCep(){
+    if (inputCep.value.length != 8){
         alert('[Erro] Verifique o CEP e tente novamente!')
     }
     else{
         apagarResultado(div)
-        api(input.value, div)
+        apiCep(inputCep.value, div)
+        inputCep.value = ''
+    }
+}
+
+function verificarRua(){
+    if (inputEstado.value.length != 2){
+        alert('[Erro] Verifique o Estado e tente novamente!')
+        console.warn('[Erro] O estado esta incorreto, por favor, corrija!')
+    }
+    else{
+        apagarResultado(div)
+        apiRua(inputEstado.value, inputCidade.value, inputRua.value, div)
+        inputRua.value = ''
+        inputCidade.value = ''
+        inputEstado.value = ''
     }
 }
